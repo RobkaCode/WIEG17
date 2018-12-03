8b18c5c3b2077d0dc1fbf9664d3ff328710f05b0
RobkaCode <45166669+RobkaCode@users.noreply.github.com>
2018-12-03 13:38:29 +0100

    Create xhr.js

---
+++ XMLHttpRequest/xhr.js
@@ -0,0 +1,49 @@
+
+var url2 = "http://api.openweathermap.org/data/2.5/weather?q=London&appid=d7ece5ce9d067035dec2e756f1368dfb"
+
+var info = document.getElementById('div1');
+var button = document.getElementById('btn')
+
+var clickCount = 1;
+
+button.addEventListener("click", function(){
+    var req = new XMLHttpRequest();
+    req.open('GET', url2);
+
+    req.onload = function(){
+    var data = req.responseText;
+    var parsedData = JSON.parse(data);
+    console.log(data);
+    sendHTML(parsedData);
+    };
+    req.send();
+});
+
+function sendHTML(inData){
+    var test = "";
+    
+    if(clickCount == 1){
+        test += ("<p>" + inData.main.temp + "</p>");
+        info.insertAdjacentHTML('beforeend', "Temperatur i London: " + test);
+    }
+    else if(clickCount == 2){
+        test += ("<p>" + inData.wind.speed + "</p>");
+        info.insertAdjacentHTML('beforeend', "Vindhastighet: " + test);
+    }
+    else if(clickCount == 3){
+        test += ("<p>" + inData.main.humidity + "</p>");
+        info.insertAdjacentHTML('beforeend', "Luftfuktighet: " + test);
+    }
+    else if(clickCount == 4){
+        test += ("<p>" + inData.main.pressure + "</p>");
+        info.insertAdjacentHTML('beforeend', "Lufttryck: " + test);
+    }
+    
+    //test += ("<p>" + inData.main.temp + "</p>");
+
+    clickCount += 1;
+
+}
+
+
+
